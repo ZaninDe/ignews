50c09eec8afc5dabd4f3d420fcd56aa9e0b021df
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-04-04 16:00:00 -0300

    add prismic env

---
+++ .slicemachine/assets/slices/MySlices/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MySlices';
+
+export default {
+  title: 'slices/MySlices'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"my_slices","items":[],"primary":{"title":[{"type":"heading1","text":"Morph bricks-and-clicks relationships","spans":[]}],"description":[{"type":"paragraph","text":"Adipisicing elit aute mollit cupidatat qui laboris laboris id sunt.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
