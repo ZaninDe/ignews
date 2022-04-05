1fdca2998ad73a429e09372765ee234bd9157fe6
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-04-05 18:59:31 -0300

    get subscription session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,17 +2,24 @@ import { useSession, signIn } from 'next-auth/react'
 import styles from './styles.module.scss'
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
+import Router, { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  const { data: session }= useSession();
+  const { data: session }= useSession()
+  const router = useRouter()
 
   async function handleSubscribe() {
     if(!session) {
       signIn('github')
+      return
+    }
+
+    if(session.activeSubscription) {
+      router.push('/posts')
       return
     }
 
