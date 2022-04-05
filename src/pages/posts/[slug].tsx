a498aa05420088ef3739767c50352db7c19ebca6
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-04-05 19:41:12 -0300

    add preview posts

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -42,7 +42,7 @@ export const getServerSideProps: GetServerSideProps = async ({req, params}) => {
 
   console.log(session)
   
-  if(!session.activeSubscription) {
+  if(!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
